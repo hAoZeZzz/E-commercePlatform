a15b42664b878e717ac9ee7c513cb5cecdb58260
hAoZeZzz <haoze883@gmail.com>
2024-08-23 22:36:50 +1000

    Set up Homepage

---
+++ Frontend/ordermanagement/src/app/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import React from 'react';
+import NavBar from './nav/NavBar';
+import { Container } from '@mui/material';
+
+export default function Layout() {
+    return(
+        <>
+            <NavBar />
+            <Container sx={{ p:'2rem'}}>
+                <Outlet />
+            </Container>
+        </>
+    )
+}
